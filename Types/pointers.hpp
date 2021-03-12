c5131310a530adc7f192a5229bcc3f9cc1744cbc
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-03-12 17:25:42 +0000

    Modified files - added declarations for unique & smart pointers

--- Types/pointers.hpp
+++ Types/pointers.hpp
@@ -2,6 +2,79 @@
 #define POINTERS_HPP
 #pragma once
 
-namespace Salih::Types {}
+#include <iostream>
+#include <cstdlib>
+
+namespace Salih::Types {
+
+	template<class T>
+	class Pointer<T> ;
+	
+	template<class T>
+	class SharedPointer<T> ;
+	
+	template<class T>
+	class UniquePointer<T> ;
+	
+	template<class T>
+	class Pointer<T> {
+		private:
+			T* pointer ;
+			
+			int count ; int& refCount ;
+			
+			float dataSize ;
+		public:
+			Pointer(T) ;
+			
+			Pointer(T*) ;
+			
+			T operator->(const int) const ;
+			
+			virtual ~Pointer() ;
+	} ;
+	
+	template<class T>
+	class SharedPointer : public Pointer<T> {
+		public:
+			SharedPointer(T) ;
+			
+			SharedPointer(T*) ;
+		
+			SharedPointer(const SharedPointer&) ;
+			
+			SharedPointer(SharedPointer&&) ;
+			
+			~SharedPointer() ;
+	} ;
+	
+	template<class T>
+	class UniquePointer : public Pointer<T> {
+		public:
+			UniquePointer(T) ;
+			
+			UniquePointer(T*) ;
+		
+			UniquePointer(const UniquePointer&) = delete ;
+			
+			UniquePointer(UniquePointer&&) ;
+			
+			~UniquePointer() ;
+	} ;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, Salih::Types::SharedPointer<T>& ptr)
+{
+	os << ptr->pointer ;
+	return os ;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, Salih::Types::UniquePointer<T>& ptr)
+{
+	os << ptr->pointer ;
+	return os ;
+}
 
 #endif
