15f055439f0a38cc2730f21ae510f7592a3d7426
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-03-13 17:08:44 +0000

    Modified files - provided basic implementations for smart pointers (constructors, copy constructors, destructors)

--- Types/pointers.hpp
+++ Types/pointers.hpp
@@ -7,39 +7,47 @@
 
 namespace Salih::Types {
 
-	template<class T>
-	class Pointer<T> ;
+	template<typename T>
+	class Pointer ;
+	
+	template<typename T>
+	class SharedPointer ;
+	
+	template<typename T>
+	class UniquePointer ;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream&, const Salih::Types::Pointer<T>&) ;
+
+namespace Salih::Types {
 	
 	template<class T>
-	class SharedPointer<T> ;
-	
-	template<class T>
-	class UniquePointer<T> ;
-	
-	template<class T>
-	class Pointer<T> {
-		private:
-			T* pointer ;
-			
-			int count ; int& refCount ;
-			
-			float dataSize ;
+	class Pointer {
+		protected:
+			T* pointer ;			
 		public:
+			Pointer() ;	
+	
 			Pointer(T) ;
 			
-			Pointer(T*) ;
+			T* operator->() ;
 			
-			T operator->(const int) const ;
+			T& operator*() ;
 			
-			virtual ~Pointer() ;
+			T* get() const ;
+			
+			virtual ~Pointer() = 0 ;
 	} ;
 	
 	template<class T>
 	class SharedPointer : public Pointer<T> {
+		private:
+			int* count ;
 		public:
+			SharedPointer() ;
+		
 			SharedPointer(T) ;
-			
-			SharedPointer(T*) ;
 		
 			SharedPointer(const SharedPointer&) ;
 			
@@ -51,9 +59,9 @@ namespace Salih::Types {
 	template<class T>
 	class UniquePointer : public Pointer<T> {
 		public:
-			UniquePointer(T) ;
+			UniquePointer() ;
 			
-			UniquePointer(T*) ;
+			UniquePointer(T) ;
 		
 			UniquePointer(const UniquePointer&) = delete ;
 			
@@ -63,12 +71,107 @@ namespace Salih::Types {
 	} ;
 }
 
+template<typename T>
+Salih::Types::Pointer<T>::Pointer() {}
+
+template<typename T>
+Salih::Types::Pointer<T>::Pointer(T data)
+{
+	this->pointer = new T ;
+	*pointer = data ;	
+}
+
+template<typename T>
+T* Salih::Types::Pointer<T>::operator->()
+{
+	return this->pointer ;
+}
+
+template<typename T>
+T& Salih::Types::Pointer<T>::operator*()
+{
+	return this->*pointer ;
+}
+
+template<typename T>
+T* Salih::Types::Pointer<T>::get() const
+{
+	return this->pointer ;
+}
+
+template<typename T>
+Salih::Types::Pointer<T>::~Pointer() {} ;
+
+template<typename T>
+Salih::Types::SharedPointer<T>::SharedPointer() : Salih::Types::Pointer<T>()
+{}
+
+template<typename T>
+Salih::Types::SharedPointer<T>::SharedPointer(T data) : Salih::Types::Pointer<T>(data)
+{
+	this->count = new int ;
+	*(this->count) = 1 ;
+}
+
+template<typename T>
+Salih::Types::SharedPointer<T>::SharedPointer(const Salih::Types::SharedPointer<T>& ptr)
+{
+	this->pointer = ptr.pointer ;
+	this->count = ptr.count ;
+	*count = *(this->count) + 1 ;
+}
+
+template<typename T>
+Salih::Types::SharedPointer<T>::SharedPointer(Salih::Types::SharedPointer<T>&& ptr)
+{
+	this->pointer = ptr.pointer ;
+	this->count = ptr.count ;
+	ptr.pointer = NULL ;
+	ptr.count = NULL ;
+}
+
+template<typename T>
+Salih::Types::SharedPointer<T>::~SharedPointer()
+{
+	if(*(this->count) <= 1) 
+	{
+		delete this->pointer ; this->pointer = NULL ;
+		delete this->count ; this->count = NULL ;
+	}
+	else {
+		*count = *(this->count) - 1 ;
+	}
+}
+
+template<typename T>
+Salih::Types::UniquePointer<T>::UniquePointer() : Salih::Types::Pointer<T>() {} ;
+
+template<typename T>
+Salih::Types::UniquePointer<T>::UniquePointer(T data) : Salih::Types::Pointer<T>(data) {} ;
+
+template<typename T>
+Salih::Types::UniquePointer<T>::UniquePointer(Salih::Types::UniquePointer<T>&& ptr)
+{
+	this->pointer = ptr.pointer ;
+	ptr.pointer = NULL ;
+}
+
+template<typename T>
+Salih::Types::UniquePointer<T>::~UniquePointer()
+{
+	if(this->pointer != NULL) 
+	{
+		delete this->pointer ; 
+		this->pointer = NULL ;
+	}
+}
+
 //helpful overloads
 
 template<typename T>
-std::ostream& operator<<(std::ostream& os, Salih::Types::Pointer<T>& ptr)
+std::ostream& operator<<(std::ostream& os, const Salih::Types::Pointer<T>& ptr)
 {
-	os << ptr.pointer ;
+	os << ptr.get() ;
 	return os ;
 }
 
