dfdb9f4c4ac30777d6e9480f22ee5935d8076326
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-03-21 19:16:18 +0000

    Modified files - modified test as prinicple is now implemented as a seperate (static) function

--- .testing/sources/basepointerTest.cpp
+++ .testing/sources/basepointerTest.cpp
@@ -9,73 +9,67 @@
 #include <stdexcept>
 
 #define private public
+#define protected public
 #include <lib/Types/pointers.hpp>
 using namespace Salih::Types ;
 
 struct Test 
 {
 	int val = 2 ;
-}
+} ;
 
 /* Note: since this is a virtual class, this file will only test full inherited methods aswell as 'principles'
- * These methods will be accessed via both child classes - unique and shared pointers */
- 
-TEST_CASE("Testing arrow (->) operator value - for shared pointers")
-{
-	SharedPointer<int> sp ;
-	sp = new Test ;
-	REQUIRE(sp->val = 2) ;
-}
+ * These methods will be accessed interchangeably between both child classes - the methods are identical */
 
-TEST_CASE("Testing arrow (->) operator value - for unique pointers")
+TEST_CASE("Testing arrow (->) operator value")
 {
-	UniquePointer<int> up ;
+	UniquePointer<Test> up ;
 	up = new Test ;
-	REQUIRE(sp->val = 2) ;
+	REQUIRE(up->val == 2) ;
 }
 
-TEST_CASE("Testing dereferncing (*) operator - for shared pointers")
+TEST_CASE("Testing dereferncing (*) operator")
 {
 	SharedPointer<int> sp ;
-	sp = new Test ;
-	*(sp).val = 2 ;
+	sp = new int ;
+	*sp = 2 ;
+	REQUIRE(*sp == 2) ;
 }
 
-TEST_CASE("Testing dereferncing (*) operator - for unique pointers")
-{
-	UniquePointer<int> up ;
-	up = new Test ;
-	*(up).val = 2 ;
-}
-
-TEST_CASE("Testing get method - for shared pointers")
-{
-	int* p = new int ;
-	SharedPointer<int> sp ;
-	sp = p ;
-	REQUIRE(sp.get() == p) ;
-}
-
-TEST_CASE("Testing get method - for unique pointers")
+TEST_CASE("Testing get method")
 {
 	int* p = new int ;
 	UniquePointer<int> up ;
-	sp = p ;
+	up = p ;
 	REQUIRE(up.get() == p) ;
 }
 
-TEST_CASE("Testing index ([]) operator - for shared pointers")
+TEST_CASE("Testing differentiation between stack and heap pointers")
 {
-	int* p = new int ;
-	SharedPointer<int> sp ;
-	sp = p ;
-	REQUIRE(sp[0] != nullptr) ;
-}
-
-TEST_CASE("Testing index ([]) operator - for unique pointers")
-{
-	int* p = new int ;
-	UniquePointer<int> up ;
-	sp = p ;
-	REQUIRE(up[0] != nullptr) ;
+	/* As part of an effort to prevent mistakes regarding allocation, freeing etc., the user is blocked from assigning a pointer to the stack, lest an error message be thrown - this code is embedded as inline assembly code within relevant constructors / assignment operators */
+	bool status = false ;
+	bool* data = &status ;
+	//code 
+	try {
+		Pointer<bool>::checkValidity(data) ;
+	} catch(std::runtime_error)
+	{
+		status = true ;
+	}
+	
+	REQUIRE(status == true) ;
+	
+	status = true ;
+	data = new bool ;
+	//code 
+	try {
+		Pointer<bool>::checkValidity(data) ;
+	} catch(std::runtime_error)
+	{
+		status = false ;
+		delete data ;
+	}
+	
+	REQUIRE(status == true) ;
+	delete data ;
 }
