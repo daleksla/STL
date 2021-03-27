af132acd810fbe165f11be59fd13a210f913e3d5
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-03-27 17:04:29 +0000

    Modified files - allows for cross comparisons of lists of different datatypes

--- Structures/LinkedLists/linkedlist.hpp
+++ Structures/LinkedLists/linkedlist.hpp
@@ -62,6 +62,12 @@ namespace Salih::Structures::LinkedLists {
 			
 			bool operator!=(const LinkedList<T>&) const ; //method to index linked list, returns data
 			
+			template<typename OTHER>
+			bool operator==(const LinkedList<OTHER>&) const ; //method to index linked list, returns data
+			
+			template<typename OTHER>
+			bool operator!=(const LinkedList<OTHER>&) const ; //method to index linked list, returns data
+			
 			void insert(const int, T) ;
 						
 			int getSize() const ; //get size
