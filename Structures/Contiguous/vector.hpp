6970b74722ea8537c3b04d8fb96f461ae30220d9
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-03-27 16:20:46 +0000

    Created file - added implementations and definitions for constructor, append, clear, destructor method for (child) vector class

---
+++ Structures/Contiguous/vector.hpp
@@ -0,0 +1,51 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#pragma once
+
+#include "_contiguous.hpp"
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <initializer_list>
+
+/* This file contains the declarations and implementations of a array-based (child) contiguous structure
+ * It is located in the Salih, Structures, Contiguous namespaces */
+
+namespace Salih::Structures::Contiguous {
+
+	template<class T>
+	class Vector : public Contiguous<T> {
+		private:			
+			Vector(T*, const std::size_t) ;
+		public:
+			Vector() ;
+		
+			Vector(const std::size_t) ;
+		
+			Vector(const std::initializer_list<T>&) ;
+			
+			Vector& operator=(const std::initializer_list<T>&) ;
+			
+			Vector(const Vector&) ;
+			
+			Vector& operator=(const Vector&) ;
+			
+			Vector(Vector&&) ;
+			
+			Vector& operator=(Vector&&) ;			
+			
+			//template<std::size_t NEW_SIZE>
+			//Array<T,NEW_SIZE> operator()(const int x, const int y) const ;
+			
+			void append(T) ;
+			
+			void clear() ;
+			
+			~Vector() ;
+	} ;
+	
+}
+
+#include "vector.tpp"
+
+#endif
