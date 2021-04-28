744ef6aa56834637a57028489d26d106838bd9ae
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-04-28 01:02:21 +0100

    Modified files - added docstring for all public methods

--- Types/string.hpp
+++ Types/string.hpp
@@ -4,6 +4,12 @@
 
 #include <iostream>
 
+/** @brief String-class implementation file, serving as a wrapper for a c-string (character array)
+    @author Salih Mahmoud Sayed Ahmed
+    @email ahmed233@uni.coventry.ac.uk
+    @date April 2021
+**/
+
 namespace Salih
 {
 	namespace Types {
@@ -19,8 +25,10 @@ namespace Salih
 }
 
 namespace Salih::Types {
-
+	
 	class String {
+		/** This class is the String-class implementation, serving as a c-string (character array) wrapper **/
+	
 		private:
 			char* str ;
 			
@@ -28,67 +36,145 @@ namespace Salih::Types {
 					
 			String(char*, const int) ;	
 		public:
-		
+			/** Empty constructor, intialises empty string 
+			@return <initialised-object> **/
 			String() ;
 			
+			/** Regular constructor, intialises string 
+			@param a c-string to wrap
+			@return <initialised-object> **/			
 			String(const char*) ;
-			
+
+			/** Copy constructor, creates copy of a given string
+			@param a (l-value) string object
+			@return <initialised-object> **/				
 			String(const String&) ;
-			
+		
+			/** Move constructor, takes ownership of an otherwise temporary string object
+			@param an r-value string object
+			@return <initialised-object> **/				
 			String(String&&) ;
-					
+				
+			/** Destructor, frees memory and deletes string **/					
 			~String() ;
 			
+			/** Regular assignment operator, wraps around a new c-string
+			@param a c-string to wrap
+			@return reference to current object **/			
 			String& operator=(const char*) ;
 			
+			/** Copy assignment operator, creates copy of a given string
+			@param a (l-value) string object
+			@return reference to current object **/			
 			String& operator=(const String&) ;
-			
+	
+			/** Move assignment operator, takes ownership of an otherwise temporary string object
+			@param an (r-value) string object
+			@return reference to current object **/			
 			String& operator=(String&&) ;
-			
+
+			/** Concatenation operator, creates a copy of a calling string with an additional character
+			@param a character to append
+			@return a copy of the string with the appended value **/ 			
 			String operator+(char) const ; //to append char
-			
+
+			/** Concatenation operator, modifies / appends calling string with an additional character
+			@param a character to append
+			@return a reference to the calling string **/ 			
 			String& operator+=(char) ; //to append char
-			
+
+			/** Concatenation operator, creates a copy of a calling string with an additional sequence of characters
+			@param wrapped / object string 
+			@return a copy of the string with the appended value **/ 			
 			String operator+(const String&) const ; //to append another string
 			
+			/** Concatenation operator, modifies / appends calling string with data of a provided string object
+			@param wrapped / object string 
+			@return a reference to the calling string **/ 			
 			String& operator+=(const String&) ; //to append another string
-			
+
+			/** Concatenation operator, creates a copy of a calling string with an additional sequence of characters
+			@param c-string / null-terminated sequence of characters 
+			@return a copy of the string with the appended value **/ 				
 			String operator+(const char*) const ;
 			
+			/** Concatenation operator, modifies / appends calling string with an additional sequence of characters
+			@param c-string / null-terminated sequence of characters  
+			@return a reference to the calling string **/ 			
 			String& operator+=(const char*) ;
-			
+
+			/** Comparison operator, determines if string object content matches with a given (c-)string
+			@param c-string / null-terminated sequence of characters  
+			@return a boolean representing whether equality is met **/ 			
 			bool operator==(const char*) const ;
-			
+
+			/** Inequality operator, determines if string object content does not match with a given (c-)string
+			@param c-string / null-terminated sequence of characters  
+			@return a boolean representing whether equality is not met **/ 			
 			bool operator!=(const char*) const ;
-			
+
+			/** Comparison operator, determines if string object content matches with a given string object
+			@param wrapped / object string   
+			@return a boolean representing whether equality is met **/ 				
 			bool operator==(const String&) const ;
-			
+
+			/** Inequality operator, determines if string object content does not match with a given string object
+			@param wrapped / object string   
+			@return a boolean representing whether equality is not met **/ 				
 			bool operator!=(const String&) const ;
-			
+
+			/** get method, returns the c-string representation of a string
+			@return constant c-string **/ 				
 			const char* get() const ;
-			
+
+			/** getSize method, returns size of a string object
+			@return integer representing size **/ 				
 			int getSize() const ;
-			
+
+			/** Index operator, modifying string directly
+			@param integer to index string with		
+			@return reference to a given character in the string **/ 			
 			char& operator[](const int) ;
-			
+
+			/** Index operator, modifying string directly
+			@param integer to index string with		
+			@return constant reference to a given character in the string **/ 			
 			const char& operator[](const int) const ;
-			
+
+			/** capitalise method, capitalises a copy of calling object's string value
+			@return calling object copy with capitalised values (where possible) **/ 			
 			String capitalise() const ;
-			
+
+			/** upper method, upper cases entire copy of calling object's string value
+			@return calling object copy with upper-cased values (where possible) **/ 			
 			String upper() const ;
-			
+
+			/** lower method, lower cases entire copy of calling object's string value
+			@return calling object copy with lower-cased values (where possible) **/ 			
 			String lower() const ;
-			
+
+			/** isAlphaNum method, determines if all values within string object are alphanumeric
+			@return boolean indicatting if all values are alphanumeric **/ 				
 			bool isAlphaNum() const ;
-			
+
+			/** isAlpha method, determines if all values within string object are alphabetical
+			@return boolean indicatting if all values are alphabetical **/ 				
 			bool isAlpha() const ;
-			
+
+			/** isNum method, determines if all values within string object are numerical
+			@return boolean indicatting if all values are numerical **/ 				
 			bool isNum() const ;
-			
+
+			/** isUpper method, determines if all values within string object are in upper-case
+			@return boolean indicatting if all values are in upper-case **/ 				
 			bool isUpper() const ;
-			
+
+			/** isLower method, determines if all values within string object are in lower-case
+			@return boolean indicatting if all values are in lower-case **/ 				
 			bool isLower() const ;
-			
+
+			/** isCapitalised method, determines if all values within string object are in a capitalised format
+			@return boolean indicatting if all values are in a capitalised format **/ 				
 			bool isCapitalised() const ;
 			
 			friend ::std::istream& ::Salih::operator>>(::std::istream&, String&) ;
