9e2799e34be6cfd424a3b53a794056e6539b974c
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-02 18:01:23 +0100

    Modified files - added docstring comment stating purpose of collection file

---
+++ Algorithms/sorts.hpp
@@ -0,0 +1,13 @@
+#ifndef SORTS_HPP
+#define SORTS_HPP
+#pragma once
+
+/** @brief Collection of sorting algorithms file
+    @author Salih Mahmoud Sayed Ahmed
+    @email ahmed233@uni.coventry.ac.uk
+    @date May 2021
+**/
+
+#include "./Sorts/_selectionSort.hpp" /** File containing selection sort algorithm **/
+
+#endif
