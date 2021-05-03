4b43dd0275b82e7feb3db12821f92b969dd1da1e
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-03 02:26:11 +0100

    Modified files - in light of realising std::array is NOT a fixed heap declaring array, rather a fixed c-style wrapper, I adjusted my array class accordingly. 1) stopped using base class (will make redundant after I adjust vector class) and implemented class as stand-alone, 2) everythings allocated to the stack, not heap

--- Structures/Contiguous/array.hpp
+++ Structures/Contiguous/array.hpp
@@ -8,7 +8,7 @@
 #include <utility>
 #include <initializer_list>
 
-/** @brief Array-class implementation file, representing a fixed-contiguous data structure
+/** @brief Array-class implementation file, representing a C-style array wrapper
     @author Salih Mahmoud Sayed Ahmed
     @email ahmed233@uni.coventry.ac.uk
     @date April 2021
@@ -17,10 +17,11 @@
 namespace Salih::Structures::Contiguous {
 
 	template<class T, std::size_t SIZE>
-	class Array : public Contiguous<T> {
+	class Array {
 		/** This class is the Array-class implementation, serving as a c-array wrapper **/
 		private:			
-			Array(T*, const std::size_t) ;
+			T* pointer ;
+			
 		public:
 			/** Empty constructor, intialises empty array 
 			@return <initialised-object> **/
@@ -41,10 +42,22 @@ namespace Salih::Structures::Contiguous {
 			@return <initialised-object> **/	
 			Array(const Array&) ;
 			
+			/** Copy constructor, creates copy of a given array
+			@param a (l-value) array object (of different size)
+			@return <initialised-object> **/	
+			template<std::size_t DIFF>
+			Array(const Array<T, DIFF>&) ;
+			
 			/** Copy assignment operator, creates copy of a given array
 			@param a (l-value) array object
 			@return reference to current object **/
 			Array& operator=(const Array&) ;
+			
+			/** Copy assignment operator, creates copy of a given array
+			@param a (l-value) array object (of different size)
+			@return reference to current object **/
+			template<std::size_t DIFF>
+			Array& operator=(const Array<T, DIFF>&) ;
 			
 			/** Move constructor, takes ownership of an otherwise temporary array object
 			@param an r-value array object
@@ -56,13 +69,80 @@ namespace Salih::Structures::Contiguous {
 			@return reference to current object **/
 			Array& operator=(Array&&) ;			
 			
-			//template<std::size_t NEW_SIZE>
-			//Array<T,NEW_SIZE> operator()(const int x, const int y) const ;
+			/** getSize method, returns size of a structure
+			@return returns size (of data structure) **/
+			constexpr std::size_t getSize() const ;
 			
-			/** Destructor, frees memory and deletes array **/					
+			/** Index operator, modifying structure directly
+			@param integer to index structure with		
+			@return reference to a given element in the structure **/ 
+			T& operator[](const std::size_t) ; 
+	
+			/** Index operator, modifying structure directly
+			@param integer to index structure with		
+			@return constant reference to a given element in the structure **/ 		
+			const T& operator[](const std::size_t) const ; 
+			
+			/** at method, another method to modify the structure
+			@param integer to index structure with, bool (assumingly true flag as to check whether an index is valid)
+			@return reference to a given element in the structure **/ 				
+			T& at(const std::size_t, const bool check = true) ; 
+			
+			/** at method, another method to modify the structure
+			@param integer to index structure with, bool (assumingly true flag as to check whether an index is valid)
+			@return constant reference to a given element in the structure **/ 	
+			const T& at(const std::size_t, const bool check = true) const ; 
+			
+			/** Comparison operator, determines if a calling objects values matches another structure's values
+			@param array structure to compare with (of same type and size)
+			@return a boolean representing whether equality is met **/ 	
+			bool operator==(const Array&) const ;			
+			
+			/** Comparison operator, determines if a calling objects values matches another structure's values
+			@param array structure to compare with (of different type, same size)
+			@return a boolean representing whether equality is met **/ 	
+			template<typename OTHER>
+			bool operator==(const Array<OTHER, SIZE>&) const ;
+			
+			/** Comparison operator, determines if a calling objects values matches another structure's values
+			@param array structure to compare with (of same type, different size)
+			@return a boolean (will always return false, since the arrays can't have the same values) **/ 	
+			template<std::size_t DIFF>
+			bool operator==(const Array<T, DIFF>&) const ;
+			
+			/** Comparison operator, determines if a calling objects values matches another structure's values
+			@param array structure to compare with (of different type and size)
+			@return a boolean (will always return false, since the arrays can't have the same values) **/ 	
+			template<typename OTHER, std::size_t DIFF>
+			bool operator==(const Array<OTHER, DIFF>&) const ;
+			
+			/** Inequality operator, determines if a calling objects values doesn't match another structure's values
+			@param contiguous structure to compare with (of same type and size)
+			@return a boolean representing whether inequality is met **/ 	
+			bool operator!=(const Array&) const ;			
+			
+			/** Inequality operator, determines if a calling objects values doesn't match another structure's values
+			@param array structure to compare with (of different type, same size)
+			@return a boolean representing whether inequality is met **/ 	
+			template<typename OTHER>
+			bool operator!=(const Array<OTHER, SIZE>&) const ;
+			
+			/** Inequality operator, determines if a calling objects values doesn't match another structure's
+			@param array structure to compare with (of same type, different size)
+			@return a boolean representing whether inequality is met (will always return true) **/ 	
+			template<std::size_t DIFF>
+			bool operator!=(const Array<T, DIFF>&) const ;
+			
+			/** Inequality operator, determines if a calling objects values doesn't match another structure's
+			@param array structure to compare with (of different type and size)
+			@return a boolean representing whether inequality is met (will always return true) **/ 	
+			template<typename OTHER, std::size_t DIFF>
+			bool operator!=(const Array<OTHER, DIFF>&) const ;
+			
+			/** Destructor **/					
 			~Array() ;
 			
-			template<typename OTHER, std::size_t _SIZE>
+			template<class, std::size_t>
 			friend class Array ;
 	} ;
 	
