039606b0a4ecff76e63359d1774390bafcda44dc
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-06 01:33:13 +0100

    Modified files - split docstring parameter onto two lines

--- Algorithms/Searches/_linearSearch.hpp
+++ Algorithms/Searches/_linearSearch.hpp
@@ -15,7 +15,8 @@ namespace Salih::Algorithms::Searches {
 	template<typename T, typename = typename std::enable_if< Salih::Types::Traits::isList<T>::value, T >::type, typename U>
 	bool linearSearch(const T& list, const U& target)
 	/** This is a linear search algorithm, which iterates through a list until the end if need be
-	 * @param const reference to list (to be sorted), const reference of target (to be identified in list)
+	 * @param const reference to list (to be sorted)
+	 * @param const reference of target (to be identified in list)
 	 * @return boolean (indicates if supplied target was found or not) **/
 	{
 		for(int i = 0 ; i < list.getSize() ; i++) if(list[i] == target) return true ;
