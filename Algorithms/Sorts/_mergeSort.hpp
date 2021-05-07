a41374b5e1e0c6ea5b1bb55d7fbf57c31dd36cb6
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-07 04:47:54 +0100

    Modified files - modified array merge sort and merging overloads to only accept one dimensional lists

--- Algorithms/Sorts/_mergeSort.hpp
+++ Algorithms/Sorts/_mergeSort.hpp
@@ -14,8 +14,8 @@
 
 namespace Salih::Algorithms::Sorts {
 
-	template<typename T, std::size_t SIZE>
-	inline Salih::Structures::Contiguous::Array<T, SIZE+SIZE> merge(const Salih::Structures::Contiguous::Array<T, SIZE>& x, const Salih::Structures::Contiguous::Array<T, SIZE>& y, bool(*const order)(int, int) = ASCEND)
+	template<typename U, typename = typename std::enable_if<Salih::Types::Traits::getDimensions<U>::value==0,U>::type, std::size_t SIZE>
+	inline Salih::Structures::Contiguous::Array<U, SIZE+SIZE> merge(const Salih::Structures::Contiguous::Array<U, SIZE>& x, const Salih::Structures::Contiguous::Array<U, SIZE>& y, bool(*const order)(int, int) = ASCEND)
 	/** This is the merging subroutine - it works by taking two lists and adding the smallest / the largest value from each, such that the resulting list is a combined list in order
 	This subroutine is an overload specifically for two given Array class' whereby they are of the same size, which cannot use the generic list version due to the need of compile-time setup / preallocation, which other containers have no need or capability of
 	 * @param const reference to list (one of the lists)
@@ -23,7 +23,7 @@ namespace Salih::Algorithms::Sorts {
 	 * @param const function pointer, will facilitate ordering of list. Set as either ASCEND (1,...,n) or DESCEND (n,...,1)
 	 * @return list (sorted version) **/	
 	{	
-		Salih::Structures::Contiguous::Array<T, SIZE+SIZE> out ;
+		Salih::Structures::Contiguous::Array<U, SIZE+SIZE> out ;
 		std::size_t i = 0, j = 0, k = 0 ;
 		
 		while(i < x.getSize() && j < y.getSize())
@@ -55,8 +55,8 @@ namespace Salih::Algorithms::Sorts {
 		return out ;
 	}
 	
-	template<typename T, std::size_t SIZE, std::size_t DIFF>
-	inline Salih::Structures::Contiguous::Array<T, SIZE+DIFF> merge(const Salih::Structures::Contiguous::Array<T, SIZE>& x, const Salih::Structures::Contiguous::Array<T, DIFF>& y, bool(*const order)(int, int) = ASCEND)
+	template<typename U, typename = typename std::enable_if<Salih::Types::Traits::getDimensions<U>::value==0,U>::type, std::size_t SIZE, std::size_t DIFF>
+	inline Salih::Structures::Contiguous::Array<U, SIZE+DIFF> merge(const Salih::Structures::Contiguous::Array<U, SIZE>& x, const Salih::Structures::Contiguous::Array<U, DIFF>& y, bool(*const order)(int, int) = ASCEND)
 	/** This is the merging subroutine - it works by taking two lists and adding the smallest / the largest value from each, such that the resulting list is a combined list in order
 	This subroutine is an overload specifically for two given Array class' whereby they are of a different size, which cannot use the generic list version due to the need of compile-time setup / preallocation, which other containers have no need or capability of
 	 * @param const reference to list (one of the lists)
@@ -64,7 +64,7 @@ namespace Salih::Algorithms::Sorts {
 	 * @param const function pointer, will facilitate ordering of list. Set as either ASCEND (1,...,n) or DESCEND (n,...,1)
 	 * @return list (sorted version) **/	
 	{	
-		Salih::Structures::Contiguous::Array<T, SIZE+DIFF> out ;
+		Salih::Structures::Contiguous::Array<U, SIZE+DIFF> out ;
 		std::size_t i = 0, j = 0, k = 0 ;
 		
 		while(i < x.getSize() && j < y.getSize())
@@ -158,16 +158,17 @@ namespace Salih::Algorithms::Sorts {
 		return merge(a, b, order) ;
 	}
 	
-	template<typename T, std::size_t SIZE>
-	Salih::Structures::Contiguous::Array<T, SIZE> mergeSort(const Salih::Structures::Contiguous::Array<T, SIZE>& data, bool(*const order)(int, int) = ASCEND)
+	template<typename U, typename = typename std::enable_if<Salih::Types::Traits::getDimensions<U>::value==0,U>::type, std::size_t SIZE>
+	Salih::Structures::Contiguous::Array<U, SIZE> mergeSort(const Salih::Structures::Contiguous::Array<U, SIZE>& data, bool(*const order)(int, int) = ASCEND)
 	/** This is the merge sort algorithm. It works splitting a list into minimal sublists and combining two, in order, into a bigger list. It keeps merging the lists together, then the merged lists together, until a fully sorted list is formed
+	This algorithm is an overload specifically for a given Array class', which cannot use the generic list version due to the need of compile-time setup / preallocation, which other containers have no need or capability of
 	 * @param const refernece to list (to be sorted)
 	 * @param const function pointer, will facilitate ordering of list. Set as either ASCEND (1,...,n) or DESCEND (n,...,1)
 	 * @return list (sorted version) **/	
-	{			
+	{				
 		if(SIZE <= 1) return data ;
 		
-		Salih::Structures::Contiguous::Array<T,SIZE/2> a, b ;
+		Salih::Structures::Contiguous::Array<U,SIZE/2> a, b ;
 		
 		for(std::size_t i = 0 ; i < std::size_t(SIZE / 2) ; i++) a[i] = data[i] ;
 		for(std::size_t i = std::size_t(SIZE / 2) ; i < SIZE ; i++) b[i-std::size_t(SIZE/2)] = data[i] ;
