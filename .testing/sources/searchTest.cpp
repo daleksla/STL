5a39596af257e57f7c0b00298c0c3539c5a09190
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-10 23:40:37 +0100

    Modified files - added tests for interpolation search, to see whether it finds target in unevenly distrubuted set. added test cases when algorithms are used on forward lists

--- .testing/sources/searchTest.cpp
+++ .testing/sources/searchTest.cpp
@@ -13,6 +13,7 @@
 typedef Salih::Structures::LinkedLists::List<int> intList ;
 typedef Salih::Structures::Contiguous::Array<float,4> floatArr ;
 typedef Salih::Structures::Contiguous::Vector<float> floatVec ;
+typedef Salih::Structures::LinkedLists::FwdList<double> doubleFwdList ;
 
 /* linear search */
 TEST_CASE("linear search - does it find valid value at start of list?")
@@ -20,9 +21,11 @@ TEST_CASE("linear search - does it find valid value at start of list?")
 	intList list1{1,3,2} ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list1,1) == true) ;
 	floatVec list2{3,6,4,5} ;
-	REQUIRE(Salih::Algorithms::Searches::linearSearch(list2,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list2,3) == true) ;
 	floatArr list3{3,6,4,5} ;
-	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,3) == true) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,3) == true) ;
 }
 
 TEST_CASE("linear search - does it find valid value in middle of list?")
@@ -35,6 +38,9 @@ TEST_CASE("linear search - does it find valid value in middle of list?")
 	floatArr list3{3,6,4,5} ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,6) == true) ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,4) == true) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,6) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,4) == true) ;
 }
 
 TEST_CASE("linear search - does it find valid value at end of list?")
@@ -45,6 +51,8 @@ TEST_CASE("linear search - does it find valid value at end of list?")
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list2,5) == true) ;
 	floatArr list3{3,6,4,5} ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,5) == true) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,5) == true) ;
 }
 
 TEST_CASE("linear search - does it find invalid value in list?")
@@ -55,6 +63,8 @@ TEST_CASE("linear search - does it find invalid value in list?")
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list2,20) == false) ;
 	floatArr list3{3,6,4,5} ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,30) == false) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,30) == false) ;
 }
 
 TEST_CASE("linear search - does it find value in empty list?")
@@ -65,6 +75,8 @@ TEST_CASE("linear search - does it find value in empty list?")
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list2,5) == false) ;
 	floatArr list3{} ;
 	REQUIRE(Salih::Algorithms::Searches::linearSearch(list3,5) == false) ;
+	doubleFwdList list4{} ;
+	REQUIRE(Salih::Algorithms::Searches::linearSearch(list4,5) == false) ;
 }
 
 /* binary search */
@@ -73,24 +85,31 @@ TEST_CASE("binary search - does it find valid value at start of list?")
 	intList list1{1,2,3} ;
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,1) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,3) == true) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,3) == true) ;
 }
 
-TEST_CASE("binary search - does it find valid value in middle of list?")
+TEST_CASE("binary search - does it find valid value(s) in middle of list?")
 {
 	intList list1{1,2,3} ;
-	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,3) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,2) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,6) == true) ;
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,4) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,5) == true) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,4) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,5) == true) ;
 }
 
 TEST_CASE("binary search - does it find valid value at end of list?")
 {
 	intList list1{1,2,3} ;
-	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,2) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,3) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,6) == true) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,6) == true) ;	
 }
 
 TEST_CASE("binary search - does it find invalid value in list?")
@@ -99,6 +118,8 @@ TEST_CASE("binary search - does it find invalid value in list?")
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,10) == false) ;
 	floatVec list2{3,4,5,6} ;
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,20) == false) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,30) == true) ;
 }
 
 TEST_CASE("binary search - does it find value in empty list?")
@@ -107,6 +128,8 @@ TEST_CASE("binary search - does it find value in empty list?")
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list1,2) == false) ;
 	floatVec list2{} ;
 	REQUIRE(Salih::Algorithms::Searches::binarySearch(list2,5) == false) ;
+	doubleFwdList list4{} ;
+	REQUIRE(Salih::Algorithms::Searches::binarySearch(list4,30) == true) ;
 }
 
 /* interpolation search */
@@ -115,24 +138,31 @@ TEST_CASE("interpolation search - does it find valid value at start of list?")
 	intList list1{1,2,3} ;
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,1) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,3) == true) ;
+	doubleFwdList list4{3,6,4,5} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,3) == true) ;
 }
 
 TEST_CASE("interpolation search - does it find valid value in middle of list?")
 {
 	intList list1{1,2,3} ;
-	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,3) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,2) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,6) == true) ;
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,4) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,5) == true) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,4) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,5) == true) ;
 }
 
 TEST_CASE("interpolation search - does it find valid value at end of list?")
 {
 	intList list1{1,2,3} ;
-	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,2) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,3) == true) ;
 	floatVec list2{3,4,5,6} ;
-	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,5) == true) ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,6) == true) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,6) == true) ;
 }
 
 TEST_CASE("interpolation search - does it find invalid value in list?")
@@ -141,6 +171,8 @@ TEST_CASE("interpolation search - does it find invalid value in list?")
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,10) == false) ;
 	floatVec list2{3,4,5,6} ;
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,20) == false) ;
+	doubleFwdList list4{3,4,5,6} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,30) == true) ;
 }
 
 TEST_CASE("interpolation search - does it find value in empty list?")
@@ -149,4 +181,16 @@ TEST_CASE("interpolation search - does it find value in empty list?")
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,2) == false) ;
 	floatVec list2{} ;
 	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,5) == false) ;
+	doubleFwdList list4{} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,30) == true) ;
+}
+
+TEST_CASE("interpolation search - does it find in unevenly (yet ordered) list?")
+{
+	intList list1{1,2,4,5,8,11,12} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list1,8) == true) ;
+	floatVec list2{1,2,4,5,8,11,12} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list2,8) == false) ;
+	doubleFwdList list4{1,2,4,5,8,11,12} ;
+	REQUIRE(Salih::Algorithms::Searches::interpolationSearch(list4,8) == true) ;
 }
