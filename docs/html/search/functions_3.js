fdd96c8215446256219e6886d93f283b3e87babe
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-30 03:24:17 +0100

    Modified files - updated documentation

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['del_181',['del',['../classSalih_1_1Structures_1_1Contiguous_1_1Vector.html#afeef6d5a8b74c83bbc088c4351e70ca7',1,'Salih::Structures::Contiguous::Vector::del()'],['../classSalih_1_1Structures_1_1LinkedLists_1_1FwdList.html#a085b57000ce414346733a8ead29963b4',1,'Salih::Structures::LinkedLists::FwdList::del()'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#a5ed0e4ea8f7bd2bb8751e84b69da27c7',1,'Salih::Structures::LinkedLists::List::del()']]],
+  ['del_181',['del',['../classSalih_1_1Structures_1_1Contiguous_1_1Vector.html#afeef6d5a8b74c83bbc088c4351e70ca7',1,'Salih::Structures::Contiguous::Vector::del()'],['../classSalih_1_1Structures_1_1LinkedLists_1_1FwdList.html#afff35083bf90981505e1994b6ec7f491',1,'Salih::Structures::LinkedLists::FwdList::del()'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#aa9d6a58b2dead754d346a4e40d0dd9e5',1,'Salih::Structures::LinkedLists::List::del()']]],
   ['descend_182',['DESCEND',['../namespaceSalih_1_1Algorithms_1_1Sorts.html#af739bdc2d0bb02a7cb1d0b3ce513c73b',1,'Salih::Algorithms::Sorts']]]
 ];
