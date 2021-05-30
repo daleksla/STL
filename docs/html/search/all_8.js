94ca73df0acdfe106ff5d50103623435b768f6f9
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-30 16:49:40 +0100

    Modified files - updated documentation

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['linearsearch_58',['linearSearch',['../namespaceSalih_1_1Algorithms_1_1Searches.html#a48b35a30150a63b8aba4e955b0a3bd15',1,'Salih::Algorithms::Searches']]],
+  ['linearsearch_58',['linearSearch',['../namespaceSalih_1_1Algorithms_1_1Searches.html#a438849e16d15ccd4f77a10ffc6da1b8f',1,'Salih::Algorithms::Searches::linearSearch(const UniDirectionalIterator &amp;begin, const UniDirectionalIterator &amp;end, const U &amp;target)'],['../namespaceSalih_1_1Algorithms_1_1Searches.html#aa56e8cb7972435773329fcdeb8f81fbb',1,'Salih::Algorithms::Searches::linearSearch(const T &amp;list, const U &amp;target)']]],
   ['list_59',['List',['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html',1,'Salih::Structures::LinkedLists::List&lt; T &gt;'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#a060ad3dfde46a2ffc4b5964048726df6',1,'Salih::Structures::LinkedLists::List::List()'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#a9c3c5e47e7837516f7c6f8cf861cff59',1,'Salih::Structures::LinkedLists::List::List(const List &amp;)'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#a5c2128c05cb96a20a0f0d95fa336263b',1,'Salih::Structures::LinkedLists::List::List(const std::initializer_list&lt; T &gt; &amp;)'],['../classSalih_1_1Structures_1_1LinkedLists_1_1List.html#ab13116efcb1f9858750bc3c744213ab2',1,'Salih::Structures::LinkedLists::List::List(List &amp;&amp;)']]],
   ['lower_60',['lower',['../classSalih_1_1Types_1_1String.html#ab7564f430cf3508cba0faf7c392308f4',1,'Salih::Types::String']]]
 ];
