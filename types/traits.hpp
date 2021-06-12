6177f1f050b3a8e75c1d41af15bf9e8ba32d3043
Salih <ahmed233@uni.coventry.ac.uk>
2021-06-12 15:44:48 +0100

    Modified files - added boolean operators to allow for easier usage

--- types/traits.hpp
+++ types/traits.hpp
@@ -4,7 +4,7 @@
 
 #include "macros.hpp"
 
-/** @brief Type traits allowing for compile-time type checkings, which can be used in conditional building, assertions, etc.
+/** @brief Collection of general type traits (pseudo-functionalities conveying information of given data-types)
     @author Salih Mahmoud Sayed Ahmed
     @email ahmed233@uni.coventry.ac.uk
     @date Jun 2021
@@ -19,9 +19,14 @@ namespace types {
 			static CONSTEXPRCXX11 bool value = true ;
 			
 		public:
-			inline CONSTEXPRCXX11 bool operator()
+			inline CONSTEXPRCXX11 operator bool()
 			{
 				return value ;
+			}
+			
+			inline CONSTEXPRCXX11 bool operator!()
+			{
+				return !value ;
 			}
 			
 	} ;
@@ -35,6 +40,11 @@ namespace types {
 			inline CONSTEXPRCXX11 bool operator()
 			{
 				return value ;
+			}
+			
+			inline CONSTEXPRCXX11 bool operator!()
+			{
+				return !value ;
 			}
 			
 	} ;
